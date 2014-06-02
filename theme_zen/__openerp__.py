4a171df075c21dc3c208e2fc2ec997fd998fc678
Antony Lesuisse <lesuisse@gmail.com>
2014-06-02 16:53:32 +0200

    initial commit

---
+++ theme_zen/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Theme Zen',
+    'category': 'Website',
+    'version': '1.0',
+    'author': 'Christine Derville',
+    'depends': ['website'],
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/snippets.xml',
+        'views/images.xml',
+        'views/options.xml',
+    ],
+}
