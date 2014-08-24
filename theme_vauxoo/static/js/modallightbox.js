dd9fc95c8ff0bf0741a734e0578a9874f8726550
Nhomar - Vauxoo <nhomar@gmail.com>
2014-08-24 16:19:29 -0430

    [REM] Moved theme_vauxoo to cms

--- theme_vauxoo/static/js/modallightbox.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-$('.thumbnail').click(function(){
-  	$('.modal-body').empty();
-  	var title = $(this).parent('a').attr("title");
-  	$('.modal-title').html(title);
-  	$($(this).parents('div').html()).appendTo('.modal-body');
-  	$('#myModal').modal({show:true});
-});
